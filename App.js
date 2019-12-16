c7d8b3329fe90cede80fd518f79434ff04f41cb7
liamwynne <liamdwynne5753@gmail.com>
2019-12-16 18:52:14 -0500

    adding react files

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import SimpsonsCard from "./components/SimpsonsCard";
+import Wrapper from "./components/Wrapper";
+import simpsons from "./simpsons.json";
+
+class App extends Component{
+state ={
+  simpsons
+};
+
+}
+
+
+export default App;

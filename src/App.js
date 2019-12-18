927d6987d29e3266e026f625d17f18b36cf46709
liamwynne <liamdwynne5753@gmail.com>
2019-12-18 09:59:29 -0500

    on click function is working, now time to get them to shuffle

--- src/App.js
+++ src/App.js
@@ -8,16 +8,28 @@ class App extends Component {
   state = {
     simpsons
   };
-
+  randomize = (id) => {
+    console.log("Clicked!")
+    console.log(id)
+ if (id === true) {
+     const simpsons = this.id.shuffle
+    }
+  }
   render() {
     return (
       <Wrapper>
         <Title>Simpsons Memory Game</Title>
-        {this.state.simpsons.map(characters => (
-          <SimpsonsCard
-            image={characters.image} />
+        <div className="row">
+          {this.state.simpsons.map(characters => (
+            <SimpsonsCard
+              image={characters.image} 
+              id={characters.id}
+              key={characters.id}
+              randomize={() => this.randomize(characters.id)}
+              />
 
-        ))}
+          ))}
+        </div>
       </Wrapper>
     )
   }
--- src/components/SimpsonsCard/index.js
+++ src/components/SimpsonsCard/index.js
@@ -3,6 +3,13 @@ import "./style.css";
 
 
 function SimpsonsCard(props) {
-    return <img src={props.image} />;
+    return (
+        <div className="col s3" >
+            <div className="card" onClick={props.randomize}>
+                <img alt={props.image} src={props.image} className="img-container" />
+            </div>
+        </div>
+    )
+
 }
 export default SimpsonsCard;

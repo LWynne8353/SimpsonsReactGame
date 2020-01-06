be36f04533c94e566fc2cf463f385ff46ee063b0
liamwynne <liamdwynne5753@gmail.com>
2020-01-06 15:25:49 -0500

    Updates

---
+++ precache-manifest.89061d441b72bc3c4caaf5555b318b80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32c971f3338e6c536d2a26a5c0024b48",
+    "url": "/SimpsonsReactGame/index.html"
+  },
+  {
+    "revision": "bd19db0f11e34e0bf62f",
+    "url": "/SimpsonsReactGame/static/css/main.43f65287.chunk.css"
+  },
+  {
+    "revision": "3bd87e5a2dcdf8553863",
+    "url": "/SimpsonsReactGame/static/js/2.24f315cd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/SimpsonsReactGame/static/js/2.24f315cd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bd19db0f11e34e0bf62f",
+    "url": "/SimpsonsReactGame/static/js/main.f4ff87bf.chunk.js"
+  },
+  {
+    "revision": "b70f0e4f341e109dbc4a",
+    "url": "/SimpsonsReactGame/static/js/runtime-main.6386d875.js"
+  }
+]);
